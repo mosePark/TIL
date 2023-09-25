f47ed49c577e685b521e4e51d5e502dd928c7161
mosePark <58103517+mosePark@users.noreply.github.com>
2023-09-25 15:10:51 +0900

    Update submit_2.R

--- Non-parametirc-stats/2-week/submit_2.R
+++ Non-parametirc-stats/2-week/submit_2.R
@@ -3,6 +3,8 @@ library(glmnet)
 library(caret)
 library(ggplot2)
 
+
+set.seed(1)
 ###############################################################################
 ''' 스케일, 절편 및 패널티 처리에 주의하여,
 함수 casl_lm_ridge가 특정 λ 값에 대해
@@ -55,10 +57,24 @@ glmnet_ridge$beta
 10-fold 교차 검증을 수행하는 cv.ridge_reg 함수를 만드세요.
 '''
 ###############################################################################
+# dataset
+n <- 200; p <- 4; N <- 500; M <- 20
+beta <- c(1, -1, 0.5, 0)
+mu <- rep(0, p)
+Sigma <- matrix(0.9, nrow = p, ncol = p)
+diag(Sigma) <- 1
+X <- MASS::mvrnorm(n, mu, Sigma)
+y <- X %*% beta + rnorm(n, sd = 5)
+
+# gird serach
+lam <- seq(0, 10, length = N)
+
 cv.ridge_reg <- function(X, y, lambda_vals, k)
 {
   results <- list()  # 결과를 저장할 리스트 초기화
-  
+  MSE <- rep(k, 0)
+  Lam <- rep(k, 0)
+  # k-fold
   for (i in 1:k)
   {
     folds <- caret::createFolds(1:nrow(X), k = k)
@@ -69,27 +85,24 @@ cv.ridge_reg <- function(X, y, lambda_vals, k)
     y_test <- y[folds[[i]], ]
     
     # Training
-    svd_obj <- svd(X_train)
-    U <- svd_obj$u
-    V <- svd_obj$v
-    svals <- svd_obj$d
-    l <- length(lambda_vals)
-    ridge_beta <- matrix(NA_real_, nrow = l, ncol = ncol(X))
-    for (j in seq_len(l))
-    {
-      D <- diag(svals / (svals^2 + lambda_vals[j]))
-      ridge_beta[j,] <- V %*% D %*% t(U) %*% y_train
-    }
+    ridge_beta <- casl_lm_ridge(X_train, y_train, lambda_vals)
     
     # Test
-    y_hat <- tcrossprod(X_test, ridge_beta)
-    mse <- apply((y_hat - y_test)^2, 2, mean)
+    y_hat <- X_test %*% t(ridge_beta)
+    fold_mse <- rep(0, N)
     
-    # 결과를 리스트에 추가
-    results[[i]] <- list(beta = ridge_beta, lam = lambda_vals[which.min(mse)], k = i)
+    for (j in 1:N)
+    {
+      y_pred <- as.matrix(y_hat[1:20, j])
+      fold_mse[j] <- apply((y_pred - y_test)^2, 2, mean) # y_pred와 y_test dimmension이 매칭이 안됌 즉, 다른 데이터셋을 넣었을 때 문제가 발생 확인할 것
+    }
+    
+    MSE[i] <- fold_mse[which.min(fold_mse)]
+    Lam[i] <- lambda_vals[which.min(fold_mse)]
   }
-  
-  return(results)  # 모든 폴드에 대한 결과를 반환
+  ans <- list(lambda = mean(Lam), MSE = mean(MSE))
+  return(ans)
 }
 
-cv.ridge_reg(X, y, lambda_vals = c(0.1, 0.01, 0.001, 0.0001), k = 10)
+result <- cv.ridge_reg(X, y, lambda_vals = lam, k = 10)
+result
